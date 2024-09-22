507d22eb269f2cd4cc5beaf3a8875427abdae74e
sachi-bharwada <sachi.bharwada@outlook.com>
2024-09-21 23:59:26 -0400

    fix images

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function rollDice(){
     for(let i = 0; i < numOfDice; i++){
         const value = Math.floor(Math.random()*6) + 1;
         values.push(value);
-        images.push(`<img src="dice_pictures/${value}.png>`);
+        images.push(`<img src="dice_pictures/${value}.png" alt="Dice ${value}">`);
     }
     diceResult.textContent = `dice: ${values.join(', ')}`;
     diceImages.innerHTML = images.join('');
